b62986f70ddc2681060b096a2a3670e2bc0f2df6
OnlyFran <fran.maderna@gmail.com>
2022-04-21 20:23:43 -0300

    30360 JS Complementario 1

--- JavaScript/java.js
+++ JavaScript/java.js
@@ -1,20 +1,9 @@
-let precioP = 150;
-let precioR = 90;
-let precioD = 50;
+let contraseña = 666;
+let ingreso = parseInt(prompt("Ingrese el código de acceso: "));
 
-alert("Para ser miembro de nuestra Web, interactuar y poder tener su cuenta activa, debe adquirir un videojuego como su primera insignia")
-let carrito = prompt("Qué juego desea adquirir antes de ingresar a la página? (Disponibles: Phasmophobia, Resident Evil 4 y Deceit.)");
-
-if(carrito.toLowerCase() == "phasmophobia"){
-    let compra = alert("Usted seleccionó el " + carrito + " por un valor de " + precioP + " pesos argentinos, más tarde procederá a abonar o se le suspenderá la cuenta.");
-}else if(carrito.toLowerCase() == "resident evil 4"){
-    let compra = alert("Usted seleccionó el " + carrito + " por un valor de " + precioR + " pesos argentinos, más tarde procederá a abonar o se le suspenderá la cuenta.");
-}else if(carrito.toLowerCase() == "deceit"){
-    let compra = alert("Usted seleccionó el " + carrito + " por un valor de " + precioD + " pesos argentinos, más tarde procederá a abonar o se le suspenderá la cuenta.");
-}else{
-    paseLibre();
+while(ingreso != 666){
+    alert("CÓDIGO INCORRECTO");
+    let ingreso = parseInt(prompt("Ingrese el código de acceso: "))
 }
 
-function paseLibre(){
-    alert("Usuario sin abono, comienza su prueba gratuita, al finalizar, su cuenta será suspendida.");
-}
+alert("Acceso Otorgado");
