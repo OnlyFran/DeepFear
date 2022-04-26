d27ec9bce664a8b40c5b63cce9a9afe97a29b5ac
OnlyFran <fran.maderna@gmail.com>
2022-04-26 19:41:08 -0300

    Primer Entrega JS 30360

--- JavaScript/java.js
+++ JavaScript/java.js
@@ -1,9 +1,27 @@
-let contraseña = 666;
-let ingreso = parseInt(prompt("Ingrese el código de acceso: "));
+let username = prompt("Ingrese su nombre de usuario:");
+let password = prompt("Ingrese la clave secreta:");
 
-while(ingreso != 666){
-    alert("CÓDIGO INCORRECTO");
-    let ingreso = parseInt(prompt("Ingrese el código de acceso: "))
+while(password != 666){
+    alert(`${username}, usted ha ingresado la clave incorrecta, intente nuevamente..`);
+    password = prompt("Ingrese la clave secreta:");
 }
 
-alert("Acceso Otorgado");
+alert(`Bienvenido ${username}, usted ha ingresado la clave "${password}" correctamente`);
+
+let donation = prompt("¿Desea contribuir a nuestro website con una donación a voluntad? si/no");
+let get = 1;
+
+while(get <= 0 && get == ""){
+    alert(`${username}, ingrese un monto válido.`);
+    get = parseInt(prompt("¿Cuanto desea donar?"));
+    alert(`Muchísimas gracias por su contribución de $${get}, disfrute lo que tenemos para ofrecerle.`);
+}
+
+if(donation == "si"){
+    get = parseInt(prompt("¿Cuanto desea donar?"));
+    alert(`Muchísimas gracias por su contribución de $${get}, le enviaremos un mail de confirmación luego, disfrute lo que tenemos para ofrecerle.`);
+}else if(donation == "no"){
+    alert("Disculpe la molestia, disfrute de lo que tenemos para ofrecerle.");
+}else if(donation == "" || donation != "si" || donation != "no"){
+    alert("Tomese un tiempo para pensarlo, mientras tanto.. ¡disfurte de nuestro sitio web!");
+}
